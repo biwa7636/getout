4d4a44e7f66bb8888944d78a883aaf820ceca06b
datafireball <binwang.cu@gmail.com>
2014-10-01 23:08:31 -0600

    add find_packages

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     author_email='binwang.cu@gmail.com',
     license='MIT',
     install_requires=['beautifulsoup4'],
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
     entry_points={
         'console_scripts':['getout=getout.command_line:main'],
     },
